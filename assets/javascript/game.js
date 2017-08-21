70cb3f30461c7279421e577f9091b2aac6d95f42
Brooks Rodriguez <brannsee@gmail.com>
2017-08-21 16:43:07 -0500

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,51 @@
-$(document).ready(function() {
+$(document).ready(function(){
 	//DEFINING the Variables determining if it needs to be an array, string, etc...
 //game gem number
 var crysCounter = Math.floor(Math.random() * 102 + 19);
+console.log(crysCounter)
 //to html
-$('#crysCounter').html(crysCounter);
+$('#random').html(crysCounter);
 //crystal number generator
 var diamond = Math.floor(Math.random() * 9 + 1);
 var quartz = Math.floor(Math.random() * 9 + 1);
 var emerald = Math.floor(Math.random() * 9 + 1);
 var fabulous = Math.floor(Math.random() * 9 + 1);
+console.log(diamond)
 //Holds user crystal total
 var userCrysCounter = 0;
-var winCount = 0;
-var loseCount = 0;
+var wins = 0;
+var losses = 0;
 
 //appending to html
-$('#winCount').html(winCount);
-$('#loseCount').html(loseCount);
+$('#winCount').html(wins);
+$('#loseCount').html(losses);
 //game start and reset
-function reset () {
+function reset(){
 	crysCounter = Math.floor(Math.random() * 102 + 19);
-	$('#crysCounter').html(crysCounter);
+	$('#random').html(crysCounter);
 //crystal random number for game start
-	diamond = Math.floor(Math.random() * 9 + 1);
-    quartz = Math.floor(Math.random() * 9 + 1);
-    emerald = Math.floor(Math.random() * 9 + 1);
-	fabulous = Math.floor(Math.random() * 9 + 1);
+	diamond= Math.floor(Math.random() * 9 + 1);
+    quartz= Math.floor(Math.random() * 9 + 1);
+    emerald= Math.floor(Math.random() * 9 + 1);
+	fabulous= Math.floor(Math.random() * 9 + 1);
 	userCrysCounter = 0;
 	$('#userTotal').html(userCrysCounter);
-	console.log()
+	console.log(userCrysCounter);
 }
 /*on click create random number and add to cryscounter, each gem must generate a single number 
 during each round until the crysCounter is matched or surpassed*/
 //diamond gem number generator and counter for w/l
 $('#diamond').on("click", function() {
 	userCrysCounter = userCrysCounter + diamond;
-	$('#userTotal').html(userCrysCounter);
+	console.log("New userCrysCounter= " + userCrysCounter);
+	$('#userTotal').html(userCrysCounter); //
 	if (userCrysCounter === crysCounter) {
-		winCount ++;
+		wins ++;
+		reset ();
 	} else if (userCrysCounter > crysCounter) {
-		loseCount ++;
-	} reset ();
+		losses ++;
+		reset ();
+	} 
 });
 //quartz
 $('#quartz').on("click", function() {
@@ -48,29 +53,35 @@ $('#quartz').on("click", function() {
 	console.log("New userCrysCounter=" + crysCounter);
 	$('#userTotal').html(userCrysCounter);
 	if (userCrysCounter === crysCounter) {
-		winCount ++;
+		wins ++;
+		reset ();
 	} else if (userCrysCounter > crysCounter) {
-		loseCount ++;
-	} reset ();
+		losses ++;
+		reset ();
+	} 
 });
 //emerald
 $('#emerald').on("click", function () {
 	userCrysCounter = userCrysCounter + emerald;
 	$('#userTotal').html(userCrysCounter);
 	if (userCrysCounter === crysCounter) {
-		winCount ++;
+		wins ++;
+		reset ();
 	} else if (userCrysCounter > crysCounter){
-		loseCount ++;
-	} reset ();
+		losses ++;
+		reset ();
+	} 
 });
 //fabulous
 $('#fabulous').on("click", function () {
 	userCrysCounter = userCrysCounter + fabulous;
 	$('#userTotal').html(userCrysCounter);
 	if (userCrysCounter === crysCounter) {
-		winCount ++;
+		wins ++;
+		reset ();
 	} else if (userCrysCounter > crysCounter){
-		loseCount ++;
-	} reset ();
+		losses ++;
+		reset ();
+	} 
 });
 });
